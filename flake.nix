30563f1f410ef3abea483a1fd110678dff97fd4e
Clément DOUIN <clement.douin@posteo.net>
2023-04-25 22:09:16 +0200

    email: remove bundled feature of the rusqlite crate

--- flake.nix
+++ flake.nix
@@ -8,32 +8,32 @@
       url = "github:nix-community/fenix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    naersk = {
-      url = "github:nix-community/naersk";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
-  outputs = { self, nixpkgs, flake-utils, fenix, naersk, ... }:
-    flake-utils.lib.eachDefaultSystem (system: {
-      devShells.default =
-        let
-          pkgs = import nixpkgs { inherit system; };
-          rust-toolchain = fenix.packages.${system}.fromToolchainFile {
-            file = ./rust-toolchain.toml;
-            sha256 = "eMJethw5ZLrJHmoN2/l0bIyQjoTX1NsvalWSscTixpI=";
-          };
-        in
-        pkgs.mkShell {
-          buildInputs = with pkgs;
-            [
-              # Nix env
-              rnix-lsp
-              nixpkgs-fmt
-
-              # Rust env
-              rust-toolchain
-            ];
+  outputs = { self, nixpkgs, flake-utils, fenix }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        rust-toolchain = fenix.packages.${system}.fromToolchainFile {
+          file = ./rust-toolchain.toml;
+          sha256 = "eMJethw5ZLrJHmoN2/l0bIyQjoTX1NsvalWSscTixpI=";
         };
-    });
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            # Nix env
+            rnix-lsp
+            nixpkgs-fmt
+
+            # Rust env
+            rust-toolchain
+
+            # SQLite
+            sqlite
+          ];
+        };
+
+        # TODO: find a way to cargo test
+      });
 }
