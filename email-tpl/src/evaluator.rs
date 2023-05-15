48bf33cc6804997406c7c1bd59b4834cfb78ff4d
Clément DOUIN <clement.douin@posteo.net>
2023-05-15 19:48:13 +0200

    email-tpl: use Cmd instead of String

--- email-tpl/src/evaluator.rs
+++ email-tpl/src/evaluator.rs
@@ -1,5 +1,6 @@
 use lettre::Address;
 use log::warn;
+use pimalaya_process::Cmd;
 use thiserror::Error;
 
 use crate::{
@@ -24,7 +25,7 @@ pub enum Error {
 pub struct CompilerBuilder {
     /// Represents the PGP encrypt system command. Defaults to `gpg
     /// --encrypt --armor --recipient <recipient> --quiet --output -`.
-    pgp_encrypt_cmd: Option<String>,
+    pgp_encrypt_cmd: Option<Cmd>,
 
     /// Represents the PGP encrypt recipient. By default, it will take
     /// the first address found from the "To" header of the template
@@ -33,7 +34,7 @@ pub struct CompilerBuilder {
 
     /// Represents the PGP sign system command. Defaults to `gpg
     /// --sign --armor --quiet --output -`.
-    pgp_sign_cmd: Option<String>,
+    pgp_sign_cmd: Option<Cmd>,
 }
 
 impl CompilerBuilder {
@@ -41,13 +42,13 @@ impl CompilerBuilder {
         Self::default()
     }
 
-    pub fn pgp_encrypt_cmd<C: ToString>(mut self, cmd: C) -> Self {
-        self.pgp_encrypt_cmd = Some(cmd.to_string());
+    pub fn pgp_encrypt_cmd<C: Into<Cmd>>(mut self, cmd: C) -> Self {
+        self.pgp_encrypt_cmd = Some(cmd.into());
         self
     }
 
-    pub fn some_pgp_encrypt_cmd<C: ToString>(mut self, cmd: Option<C>) -> Self {
-        self.pgp_encrypt_cmd = cmd.map(|c| c.to_string());
+    pub fn some_pgp_encrypt_cmd<C: Into<Cmd>>(mut self, cmd: Option<C>) -> Self {
+        self.pgp_encrypt_cmd = cmd.map(|c| c.into());
         self
     }
 
@@ -67,13 +68,13 @@ impl CompilerBuilder {
         self
     }
 
-    pub fn pgp_sign_cmd<C: ToString>(mut self, cmd: C) -> Self {
-        self.pgp_sign_cmd = Some(cmd.to_string());
+    pub fn pgp_sign_cmd<C: Into<Cmd>>(mut self, cmd: C) -> Self {
+        self.pgp_sign_cmd = Some(cmd.into());
         self
     }
 
-    pub fn some_pgp_sign_cmd<C: ToString>(mut self, cmd: Option<C>) -> Self {
-        self.pgp_sign_cmd = cmd.map(|c| c.to_string());
+    pub fn some_pgp_sign_cmd<C: Into<Cmd>>(mut self, cmd: Option<C>) -> Self {
+        self.pgp_sign_cmd = cmd.map(|c| c.into());
         self
     }
 
@@ -85,13 +86,13 @@ impl CompilerBuilder {
             .map_err(|errs| Error::ParseTplError(errs[0].to_string()))?
             .compile(CompilerOpts {
                 pgp_encrypt_cmd: self.pgp_sign_cmd.clone().unwrap_or_else(|| {
-                    String::from("gpg --encrypt --armor --recipient <recipient> --quiet --output -")
+                    "gpg --encrypt --armor --recipient <recipient> --quiet --output -".into()
                 }),
                 pgp_encrypt_recipient: self.pgp_encrypt_recipient.clone(),
                 pgp_sign_cmd: self
                     .pgp_sign_cmd
                     .clone()
-                    .unwrap_or_else(|| String::from("gpg --sign --armor --quiet --output -")),
+                    .unwrap_or_else(|| "gpg --sign --armor --quiet --output -".into()),
             })?;
 
         Ok(mime_msg)
@@ -102,16 +103,16 @@ impl CompilerBuilder {
 /// down to the [Tpl::compile] function.
 #[derive(Clone, Debug, Default, Eq, PartialEq)]
 pub struct CompilerOpts {
-    pub pgp_encrypt_cmd: String,
+    pub pgp_encrypt_cmd: Cmd,
     pub pgp_encrypt_recipient: Option<Address>,
-    pub pgp_sign_cmd: String,
+    pub pgp_sign_cmd: Cmd,
 }
 
 impl CompilerOpts {
     /// Builds the final PGP encrypt syste command by replacing
     /// `<recipient>` occurrences with the actual recipient. Fails in
     /// case no recipient is found.
-    pub(crate) fn pgp_encrypt_cmd(&self) -> Result<String> {
+    pub(crate) fn pgp_encrypt_cmd(&self) -> Result<Cmd> {
         let recipient = self
             .pgp_encrypt_recipient
             .as_ref()
@@ -119,6 +120,7 @@ impl CompilerOpts {
 
         let cmd = self
             .pgp_encrypt_cmd
+            .clone()
             .replace("<recipient>", &recipient.to_string());
 
         Ok(cmd)
