8bf6b29e657691b2a4ff1cfc17079d27f1355918
Clément DOUIN <clement.douin@posteo.net>
2023-06-22 12:23:34 +0200

    email: add backend config and trait doc

--- email/src/backend/config.rs
+++ email/src/backend/config.rs
@@ -1,7 +1,8 @@
-//! Backend config module.
+//! Module dedicated to backend configuration.
 //!
-//! This module contains the representation of the backend
-//! configuration of the user account.
+//! This module contains the backend configuration used for the
+//! current account. One account can have only one backend and so one
+//! backend configuration.
 
 #[cfg(feature = "imap-backend")]
 use crate::ImapConfig;
@@ -9,14 +10,23 @@ use crate::MaildirConfig;
 #[cfg(feature = "notmuch-backend")]
 use crate::NotmuchConfig;
 
-/// Represents the backend configuration of the user account.
+/// The backend configuration structure.
 #[derive(Clone, Debug, Default, Eq, PartialEq)]
 pub enum BackendConfig {
+    /// The undefined backend is useful when you need to create an
+    /// account that only send emails using the
+    /// [sender](crate::Sender).
     #[default]
     None,
+
+    /// The Maildir backend configuration.
     Maildir(MaildirConfig),
+
+    /// The IMAP backend configuration.
     #[cfg(feature = "imap-backend")]
     Imap(ImapConfig),
+
+    /// The notmuch backend configuration.
     #[cfg(feature = "notmuch-backend")]
     Notmuch(NotmuchConfig),
 }
--- email/src/backend/mod.rs
+++ email/src/backend/mod.rs
@@ -1,4 +1,15 @@
-//! Module dedicated to backends.
+//! Module dedicated to backend.
+//!
+//! The core concept of this module is the [Backend] trait, which is
+//! an abstraction over emails manipulation.
+//!
+//! Then you have the [BackendConfig] which represents the
+//! backend-specific configuration, mostly used by the
+//! [account configuration](crate::AccountConfig).
+//!
+//! You also have everything you need to
+//! [synchronize](crate::backend::sync) a remote backend with a local
+//! Maildir backend.
 
 mod config;
 #[cfg(feature = "imap-backend")]
@@ -30,17 +41,50 @@ pub enum Error {
     BuildUndefinedBackendError,
 }
 
+/// The backend abstraction.
+///
+/// The backend trait abstracts every action needed to manipulate
+/// emails.
 pub trait Backend {
+    /// Returns the name of the backend.
     fn name(&self) -> String;
 
+    /// Creates the given folder.
     fn add_folder(&mut self, folder: &str) -> Result<()>;
+
+    /// Lists all available folders.
     fn list_folders(&mut self) -> Result<Folders>;
+
+    /// Expunges the given folder.
+    ///
+    /// The concept is similar to the IMAP expunge: it definitely
+    /// deletes emails that have the Deleted flag.
     fn expunge_folder(&mut self, folder: &str) -> Result<()>;
+
+    /// Purges the given folder.
+    ///
+    /// Manipulate with caution: all emails contained in the given
+    /// folder are definitely deleted.
     fn purge_folder(&mut self, folder: &str) -> Result<()>;
+
+    /// Definitely deletes the given folder.
+    ///
+    /// Manipulate with caution: all emails contained in the given
+    /// folder are also definitely deleted.
     fn delete_folder(&mut self, folder: &str) -> Result<()>;
 
+    /// Gets the envelope from the given folder matching the given id.
     fn get_envelope(&mut self, folder: &str, id: &str) -> Result<Envelope>;
+
+    /// Lists all available envelopes from the given folder matching
+    /// the given pagination.
     fn list_envelopes(&mut self, folder: &str, page_size: usize, page: usize) -> Result<Envelopes>;
+
+    /// Sorts and filters envelopes from the given folder matching the
+    /// given query, sort and pagination.
+    // TODO: we should avoid using strings for query and sort, instead
+    // it would be better to have a shared API.
+    // See https://todo.sr.ht/~soywod/pimalaya/39.
     fn search_envelopes(
         &mut self,
         folder: &str,
@@ -50,21 +94,51 @@ pub trait Backend {
         page: usize,
     ) -> Result<Envelopes>;
 
+    /// Adds the given raw email with the given flags to the given
+    /// folder.
     fn add_email(&mut self, folder: &str, email: &[u8], flags: &Flags) -> Result<String>;
+
+    /// Previews emails from the given folder matching the given ids.
+    ///
+    /// Same as `get_emails`, except that it just "previews": the Seen
+    /// flag is not applied to the corresponding envelopes.
     fn preview_emails(&mut self, folder: &str, ids: Vec<&str>) -> Result<Messages>;
+
+    /// Gets emails from the given folder matching the given ids.
     fn get_emails(&mut self, folder: &str, ids: Vec<&str>) -> Result<Messages>;
+
+    /// Copies emails from the given folder to the given folder
+    /// matching the given ids.
     fn copy_emails(&mut self, from_folder: &str, to_folder: &str, ids: Vec<&str>) -> Result<()>;
+
+    /// Moves emails from the given folder to the given folder
+    /// matching the given ids.
     fn move_emails(&mut self, from_folder: &str, to_folder: &str, ids: Vec<&str>) -> Result<()>;
+
+    /// Deletes emails from the given folder matching the given ids.
+    ///
+    /// In fact the matching emails are not deleted, they are moved to
+    /// the trash folder. If the given folder IS the trash folder,
+    /// then it adds the Deleted flag instead. Matching emails will be
+    /// definitely deleted after calling `expunge_folder`.
     fn delete_emails(&mut self, folder: &str, ids: Vec<&str>) -> Result<()>;
 
+    /// Adds the given flags to envelopes matching the given ids from
+    /// the given folder.
     fn add_flags(&mut self, folder: &str, ids: Vec<&str>, flags: &Flags) -> Result<()>;
+    /// Replaces envelopes flags matching the given ids from the given
+    /// folder.
     fn set_flags(&mut self, folder: &str, ids: Vec<&str>, flags: &Flags) -> Result<()>;
+    /// Removes the given flags to envelopes matching the given ids
+    /// from the given folder.
     fn remove_flags(&mut self, folder: &str, ids: Vec<&str>, flags: &Flags) -> Result<()>;
 
+    /// Alias for adding the Deleted flag to the matching envelopes.
     fn mark_emails_as_deleted(&mut self, folder: &str, ids: Vec<&str>) -> Result<()> {
         self.add_flags(folder, ids, &Flags::from_iter([Flag::Deleted]))
     }
 
+    /// Cleans up sessions, clients, cache etc.
     fn close(&mut self) -> Result<()> {
         Ok(())
     }
@@ -72,6 +146,10 @@ pub trait Backend {
     fn as_any(&self) -> &dyn Any;
 }
 
+/// The backend builder.
+///
+/// This builder helps you to build a `Box<dyn Backend>`. The type of
+/// backend depends on the given account configuration.
 #[derive(Clone, Debug, Default, Eq, PartialEq)]
 pub struct BackendBuilder {
     account_config: AccountConfig,
@@ -80,6 +158,7 @@ pub struct BackendBuilder {
 }
 
 impl BackendBuilder {
+    /// Creates a new builder with default value.
     pub fn new(account_config: AccountConfig) -> Self {
         Self {
             account_config,
@@ -87,11 +166,18 @@ impl BackendBuilder {
         }
     }
 
+    /// Disable cache setter.
+    pub fn disable_cache(&mut self, disable_cache: bool) {
+        self.disable_cache = disable_cache;
+    }
+
+    /// Disable cache setter following the builder pattern.
     pub fn with_cache_disabled(mut self, disable_cache: bool) -> Self {
         self.disable_cache = disable_cache;
         self
     }
 
+    /// Default credentials setter following the builder pattern.
     pub fn with_default_credentials(mut self) -> Result<Self> {
         self.default_credentials = match &self.account_config.backend {
             #[cfg(feature = "imap-backend")]
@@ -103,10 +189,7 @@ impl BackendBuilder {
         Ok(self)
     }
 
-    pub fn disable_cache(&mut self, disable_cache: bool) {
-        self.disable_cache = disable_cache;
-    }
-
+    /// Builds a [backend](crate::Backend) by cloning self options.
     pub fn build(&self) -> Result<Box<dyn Backend>> {
         match &self.account_config.backend {
             BackendConfig::None => Ok(Err(Error::BuildUndefinedBackendError)?),
@@ -138,6 +221,7 @@ impl BackendBuilder {
         }
     }
 
+    /// Builds a [backend](crate::Backend) by moving self options.
     pub fn into_build(self) -> Result<Box<dyn Backend>> {
         match self.account_config.backend.clone() {
             BackendConfig::None => Ok(Err(Error::BuildUndefinedBackendError)?),
