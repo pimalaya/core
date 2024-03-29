c582523c0121c27edbe21a72e75d048408f52fc0
Perma Alesheikh <me@prma.dev>
2024-03-29 15:22:42 +0330

    separate otu errors to itsown folder
    Just for the sake of uniformity with other libraries in the pimalaya.
    
    Signed-off-by: Perma Alesheikh <me@prma.dev>

---
+++ process/src/error.rs
@@ -0,0 +1,22 @@
+use std::{io, result, string::FromUtf8Error};
+
+use thiserror::Error;
+
+/// The global `Result` alias of the library.
+pub type Result<T> = result::Result<T, Error>;
+
+/// The global `Error` enum of the library.
+#[derive(Debug, Error)]
+pub enum Error {
+    #[error("cannot get standard input")]
+    GetStdinError,
+    #[error("cannot get exit status code of command: {0}")]
+    GetExitStatusCodeNotAvailableError(String),
+    #[error("command {0} returned non-zero exit status code {1}: {2}")]
+    GetExitStatusCodeNonZeroError(String, i32, String),
+    #[error("cannot parse command output as string")]
+    ParseOutputAsUtf8StringError(#[source] FromUtf8Error),
+
+    #[error(transparent)]
+    IoError(#[from] io::Error),
+}
--- process/src/lib.rs
+++ process/src/lib.rs
@@ -14,15 +14,14 @@
 //! 3. Commands can be executed in a pipeline, which means the output
 //! of the previous command is send as input of the next one.
 
-use log::{debug, error};
+pub mod error;
+pub use error::*;
+use log::debug;
 use std::{
-    env, io,
+    env,
     ops::{Deref, DerefMut},
     process::Stdio,
-    result,
-    string::FromUtf8Error,
 };
-use thiserror::Error;
 use tokio::{io::AsyncWriteExt, process::Command as TokioCommand};
 
 fn new_tokio_cmd() -> TokioCommand {
@@ -37,25 +36,6 @@ fn new_tokio_cmd() -> TokioCommand {
     cmd.arg(arg);
     cmd
 }
-
-/// The global `Error` enum of the library.
-#[derive(Debug, Error)]
-pub enum Error {
-    #[error("cannot get standard input")]
-    GetStdinError,
-    #[error("cannot get exit status code of command: {0}")]
-    GetExitStatusCodeNotAvailableError(String),
-    #[error("command {0} returned non-zero exit status code {1}: {2}")]
-    GetExitStatusCodeNonZeroError(String, i32, String),
-    #[error("cannot parse command output as string")]
-    ParseOutputAsUtf8StringError(#[source] FromUtf8Error),
-
-    #[error(transparent)]
-    IoError(#[from] io::Error),
-}
-
-/// The global `Result` alias of the library.
-pub type Result<T> = result::Result<T, Error>;
 
 /// The main command structure.
 ///
@@ -439,7 +419,7 @@ impl From<CommandOutput> for Vec<u8> {
 impl TryFrom<CommandOutput> for String {
     type Error = Error;
 
-    fn try_from(cmd: CommandOutput) -> result::Result<Self, Self::Error> {
+    fn try_from(cmd: CommandOutput) -> Result<Self> {
         String::from_utf8(cmd.into()).map_err(Error::ParseOutputAsUtf8StringError)
     }
 }
