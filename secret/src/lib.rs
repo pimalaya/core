49bb5e04bdfeb1a9e483fd228a2e694796b0f727
Perma Alesheikh <me@prma.dev>
2024-03-29 15:22:44 +0330

    separate otu errors of secret crate to itsown mod
    Just for the sake of uniformity with other libraries in the pimalaya.
    
    Signed-off-by: Perma Alesheikh <me@prma.dev>

--- secret/src/lib.rs
+++ secret/src/lib.rs
@@ -14,27 +14,8 @@ use log::debug;
 pub use process;
 #[cfg(feature = "command")]
 use process::Command;
-use std::result;
-use thiserror::Error;
-
-/// The global `Error` enum of the library.
-#[derive(Debug, Error)]
-pub enum Error {
-    #[error("cannot get secret: secret is not defined")]
-    GetUndefinedSecretError,
-    #[cfg(feature = "command")]
-    #[error("cannot get secret from command")]
-    GetSecretFromCommand(#[source] process::Error),
-    #[cfg(feature = "command")]
-    #[error("cannot get secret from command: output is empty")]
-    GetSecretFromCommandEmptyOutputError,
-    #[cfg(feature = "keyring")]
-    #[error("error while using secret from keyring")]
-    KeyringError(#[source] keyring::Error),
-}
-
-/// The global `Result` alias of the library.
-pub type Result<T> = result::Result<T, Error>;
+pub mod error;
+pub use error::*;
 
 /// The secret.
 ///
